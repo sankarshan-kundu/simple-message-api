dbe7b9cfc933fe51963e708d446ac2e15fddcf95
Sankarshan Kundu <hellotubai@yahoo.com>
2020-05-28 16:55:34 -0400

    added entity framework

---
+++ MessageApi/Models/Message.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MessageApi.Models
+{
+    public partial class Message
+    {
+        public int Id { get; set; }
+        public string MessageText { get; set; }
+    }
+}
---
+++ MessageApi/Models/MessageDBContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace MessageApi.Models
+{
+    public partial class MessageDBContext : DbContext
+    {
+        public MessageDBContext()
+        {
+        }
+
+        public MessageDBContext(DbContextOptions<MessageDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Message> Message { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=msg-db;Database=MessageDB;User Id=sa;Password=P@ssw0rd;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
