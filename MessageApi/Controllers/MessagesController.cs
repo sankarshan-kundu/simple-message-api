05f9cbfa428697ca02f925c499f4544a1877aa45
Sankarshan Kundu <hellotubai@yahoo.com>
2020-05-29 10:48:28 -0400

    implement all REST services

--- MessageApi/Controllers/MessagesController.cs
+++ MessageApi/Controllers/MessagesController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -37,6 +38,61 @@ namespace MessageApi.Controllers
                 return NotFound();
             }
             return message;
+        }
+
+        // POST: api/Messages
+        [HttpPost]
+        public async Task<ActionResult<Message>> PostMessage(Message msg)
+        {
+            _dBContext.Message.Add(msg);
+            await _dBContext.SaveChangesAsync();
+
+            return CreatedAtAction("GetMessage", new { id = msg.Id }, msg);
+        }
+
+        // PUT: api/Messages/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMessage(int id, Message msg)
+        {
+            if (id != msg.Id)
+            {
+                return BadRequest();
+            }
+
+            _dBContext.Entry(msg).State = EntityState.Modified;
+
+            try
+            {
+                await _dBContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_dBContext.Message.Any(m => m.Id == id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/Messages/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Message>> DeleteMessage(int id)
+        {
+            var msg = await _dBContext.Message.FindAsync(id);
+            if (msg == null)
+            {
+                return NotFound();
+            }
+            _dBContext.Message.Remove(msg);
+            await _dBContext.SaveChangesAsync();
+
+            return msg;
         }
     }
 }
